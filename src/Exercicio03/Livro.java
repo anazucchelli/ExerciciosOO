0b7016cc747284edb5fbc91791deee51a7df3fce
anazucchelli <39284021+anazucchelli@users.noreply.github.com>
2020-02-13 14:33:26 -0300

    Update Livro.java

--- src/Exercicio03/Livro.java
+++ src/Exercicio03/Livro.java
@@ -13,28 +13,5 @@ public class Livro {
         this.numPaginas = numPaginas;
     }
 
-    //get e set
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public int getNumPaginas() {
-        return numPaginas;
-    }
-
-    public void setNumPaginas(int numPaginas) {
-        this.numPaginas = numPaginas;
-    }
+    
 }
