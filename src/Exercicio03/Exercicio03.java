2d97a037e786ec11e8f3e65330e440b021f7e129
anazucchelli <39284021+anazucchelli@users.noreply.github.com>
2020-02-13 14:39:45 -0300

    Update Exercicio03.java

--- src/Exercicio03/Exercicio03.java
+++ src/Exercicio03/Exercicio03.java
@@ -1,6 +1,8 @@
 package Exercicio03;
 
 public class Exercicio03 {
-    Livro livro1 = new Livro("Brasil", "Alvares Cabral", 200);
+   public static void main(String[] args) {
+        Livro livro1 = new Livro("Brasil", "Alvares Cabral", 200);
+    }
 
 }
